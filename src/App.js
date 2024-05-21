a88a7e9cb6e026e1e74436021d7cd2ba082b28b2
guoyupei <33007697@qq.com>
2024-05-21 12:03:22 +0800

    新建入口文件，新建APP組件

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//引入react，component
+import React,{Component} from "react";
+//使用antdUI组件库
+import { Calendar, theme } from 'antd';
+const onPanelChange = (value, mode) => {
+    console.log(value.format('YYYY-MM-DD'), mode);
+};
+const { token } = theme.useToken();
+const wrapperStyle = {
+    width: 300,
+    border: `1px solid ${token.colorBorderSecondary}`,
+    borderRadius: token.borderRadiusLG,
+};
+//页面
+export default class App extends Component{
+    render() {
+        return (
+            <div>
+                <div>App</div>
+                <div style={wrapperStyle}>
+                    <Calendar fullscreen={false} onPanelChange={onPanelChange}/>
+                </div>
+            </div>
+        )
+    }
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+/*
+引入react，和component
+* */
+import React from "react";
+//引入reactDom
+import ReactDOM from 'react-dom/client'
+//引入app组件
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+//渲染APP页面
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <App/>
+)
